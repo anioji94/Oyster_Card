283d8e64f1a089d3c0d68b3eefa6b32d7b594303
jshorns <joey.s.hornsby@gmail.com>
2021-02-12 16:45:04 +0000

    added incomplete method to journey class and testing

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,15 +2,20 @@ class Journey
 	attr_reader :exit_station, :entry_station
 
 	def initialize
-		@entry_station
-		@exit_station
+		@entry_station = nil
+		@exit_station = nil
 	end
 
 	def save_entry(station)
-		@entry_station = station
+		@entry_station = station.name
 	end
 
 	def save_exit(station)
-		@exit_station = station
+		@exit_station = station.name
 	end
+
+	def incomplete?
+		(!@entry_station && !!@exit_station) || (!!@entry_station && !@exit_station) ? true : false
+	end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -14,19 +14,30 @@ describe Journey do
 			expect(subject.exit_station).to be_nil
 		end
 	end
-	
+
 	describe '#save_entry' do
 		it 'should record the entry station of a journey' do
-			expect(subject.save_entry(:entry_station)).to_not be_nil
+			expect(subject.save_entry(entry_station)).to_not be_nil
 		end
 	end
-	
+
 
 	describe '#save_exit' do
 		it 'should record the exit station of a journey' do
-			expect(subject.save_exit(:exit_station)).to_not be_nil
+			expect(subject.save_exit(exit_station)).to_not be_nil
 		end
 	end
 
-end
+	describe '#incomplete?' do
+		it 'should return true if entry saved but not exit' do
+			subject.save_entry(entry_station)
+			expect(subject.incomplete?).to be true
+		end
+		it 'should return true if exit saved but not entry' do
+			subject.save_exit(exit_station)
+			expect(subject.incomplete?).to be true
+		end
 
+	end
+
+end
